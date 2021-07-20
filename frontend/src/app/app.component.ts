fb98d1240f0b639843bded618931f74f8ffc74d1
tedsilb <26172399+tedsilb@users.noreply.github.com>
2021-07-19 23:51:42 -0500

    Move ng app to frontend/, format

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
   title = 'twooter';
